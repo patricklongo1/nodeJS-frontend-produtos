eb43073e68c4fe23750cdeb8c4d982717443c6f4
Patrick Longo <ck.longo@bol.com.br>
2019-10-20 19:28:42 -0300

    Done

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import api from '../../services/api'
+import {Link} from 'react-router-dom'
 
 import './styles.css'
 
@@ -23,15 +24,15 @@ export default class Main extends Component{
     }
 
     prevPage = () => {
-        const {page, productInfo} = this.state
-        if(page == 1) return
+        const {page} = this.state
+        if(page === 1) return
         const pageNumber = page-1
         this.loadProducts(pageNumber)
     }
 
     nextPage = () => {
         const {page, productInfo} = this.state
-        if(page == productInfo.pages) return
+        if(page === productInfo.pages) return
         const pageNumber = page+1 
         this.loadProducts(pageNumber)
     }
@@ -45,12 +46,12 @@ export default class Main extends Component{
                     <article key={product._id}>
                     <strong>{product.title}</strong>
                     <p>{product.description}</p>
-                    <a href="">Acessar</a>
+                    <Link to={`/products/${product._id}`}>Acessar</Link>
                     </article>
                 ))}
                 <div className="actions">
-                    <button disabled={page == 1} onClick={this.prevPage}>Anterior</button>
-                    <button disabled={page == productInfo.pages} onClick={this.nextPage}>Próxima</button>
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
                 </div>
             </div>
         )
